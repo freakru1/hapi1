9ea926fa4c3f1a207e06f5778eee72268329f265
freakru <o.giese@gmail.com>
2015-01-29 21:23:32 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var Hapi = require('hapi');
+var Good = require('good');
+var server = new Hapi.Server();
+
+server.connection({port: 3000});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.register({
+    register: Good,
+    options: {
+        reporters: [{
+            reporter: require('good-console'),
+            args:[{ log: '*', response: '*' }]
+        }]
+    }
+}, function (err) {
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+    server.start(function () {
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
+});
